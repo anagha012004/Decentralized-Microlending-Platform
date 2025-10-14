c458a46bbce26b7564a01a1ed1da9862fdc0dc7b
anagha012004 <anaghabadhe3@gmail.com>
2025-10-14 09:43:19 +0530

    NFT changes

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -2,10 +2,15 @@ const { ethers } = require("hardhat");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
     const NFTRewards = await ethers.getContractFactory("NFTRewards");
     const nftRewards = await NFTRewards.deploy(deployer.address);
     await nftRewards.deployed();
+
     console.log("NFTRewards deployed to:", nftRewards.address);
+    console.log("Update client/src/utils/constants.js with this address if different");
 }
 
 main().catch((error) => {
